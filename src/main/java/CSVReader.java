c8299c685698587226906dc7ed390da384bc9004
olga <opasieczynska@gmail.com>
2020-02-20 22:48:50 +0100

    init

--- src/main/java/CSVReader.java
+++ src/main/java/CSVReader.java
@@ -1,3 +1,20 @@
-public class CSVReader {
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
+public class CSVReader<T> {
+
+    public List<T> read(String filename, CSVMapper csvMapper) throws FileNotFoundException, IOException {
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(filename));
+        ArrayList<T> lines = new ArrayList<T>(); 
+        String line = null;
+        while ((line = bufferedReader.readLine()) != null) {
+            lines.add(line);
+        }
+        bufferedReader.close();
+        return lines;
+    }
 }
